7c3af16ff5f95365ed135f6c8e1a8ae12975e10b
Simagine/Sadek-Amine <cscience670@gmail.com>
2026-07-13 23:17:47 -0600

    Update animations.js

--- src/utils/animations.js
+++ src/utils/animations.js
@@ -7,6 +7,7 @@ export const animateWithGsap = (target, animationProps, scrollProps) => {
   gsap.to(target, {
     ...animationProps,
     scrollTrigger: {
+      
       trigger: target,
       toggleActions: 'restart reverse restart reverse',
       start: 'top 85%',
