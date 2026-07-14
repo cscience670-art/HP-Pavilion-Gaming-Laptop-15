eaa38b81de48b1781fdb4293ba033ee869310d17
Simagine/Sadek-Amine <cscience670@gmail.com>
2026-07-13 23:16:48 -0600

    Import additional video assets in index.js

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,6 +2,7 @@ import hero from "/assets/images/hero3.png";
 
 export const heroImg = hero;
 
+
 import hmv from "/assets/videos/hero2.mp4";
 import smallmv from "/assets/videos/smallHero.mp4";
 import highlightFirstmv from "/assets/videos/highlight-first.mp4";
